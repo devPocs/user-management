abf530b0916070a9e1efe37f67cd5a35002c76f1
Pokoh Ufuoma <pokoh.ufuoma@gmail.com>
2022-11-29 10:51:17 +0100

    made some changes to config

--- server.js
+++ server.js
@@ -15,25 +15,25 @@ const port = app.set("port", process.env.PORT || 4040)
 
 //database connection
 //local connection
-mongoose.connect("mongodb://localhost:27017/UserManagementSystemU", (err) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log("database is up and running")
-  }
-})
-//const DB = process.env.DB_CONN.replace("<PASSWORD>", process.env.PASSWORD)
+//mongoose.connect("mongodb://localhost:27017/UserManagementSystemU", (err) => {
+//  if (err) {
+//    console.log(err)
+//  } else {
+//    console.log("database is up and running")
+// }
+//})
+const DB = process.env.DB_CONN.replace("<PASSWORD>", process.env.PASSWORD)
 
-//mongoose
-// .connect(DB, {
-//   useNewUrlParser: true,
-// })
-// .then(() => {
-//   console.log("Database is up and running!")
-// })
-// .catch((err) => {
-//   console.log(err)
-// })
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Database is up and running!")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
 //add the view engine
 app.set("view engine", "ejs")
